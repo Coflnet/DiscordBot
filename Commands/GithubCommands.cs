de4f57e827540e2acecb7307d9921594f91f6e6e
Äkwav <ekwav@coflnet.com>
2025-09-09 22:24:44 +0200

    emphermeral only outside of main server

--- Commands/GithubCommands.cs
+++ Commands/GithubCommands.cs
@@ -25,7 +25,7 @@ public class GithubCommands : InteractionModuleBase
         [Summary("repo", "Repository to create the issue in"), Autocomplete<GitRepoAutocompleteHandler>()] string repo,
         [Summary("body", "Body of the issue")] string body = "")
     {
-        await DeferAsync(true);
+        await DeferAsync(Context.Interaction.GuildId != 267680588666896385);
         var callingChannel = Context.Channel;
         if (Context.User.Id != 267680402594988033)
         {
