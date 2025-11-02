6ee4667414290af51787eedbb081a054a9f16549
Äkwav <ekwav@coflnet.com>
2025-11-02 13:22:19 +0100

    not how to try again

--- Commands/Create.cs
+++ Commands/Create.cs
@@ -273,7 +273,7 @@ public partial class VpsCommands
                 {
                     var referenceId = Guid.NewGuid();
                     logger.LogInformation("Failed to get login link {lines} for {instance}, referenceId: {refernceId}", string.Join("\n", lines), newInstance.Id, referenceId);
-                    await ModifyOriginalResponseAsync(msg => msg.Content = $"Failed to get login link, please check the logs, try again or ask Äkwav to help and give him `{referenceId}`");
+                    await ModifyOriginalResponseAsync(msg => msg.Content = $"Failed to get login link, please check the logs, try again (with `/vps start`) or ask Äkwav to help and give him `{referenceId}`");
                     return false;
                 }
                 else
