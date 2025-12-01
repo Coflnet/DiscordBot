bd72540ef78b79e158c13b415034c700f18bb2e7
Äkwav <ekwav@coflnet.com>
2025-12-01 12:56:11 +0100

    truncate description

--- Commands/VpsCommands.cs
+++ Commands/VpsCommands.cs
@@ -83,7 +83,7 @@ public partial class VpsCommands : InteractionModuleBase
             await interaction.RespondAsync(new[] { new AutocompleteResult("Failed to get settings", "error") });
             return;
         }
-        var transformed = options.Where(o => !o.Value.Hide!.Value).Select(o => new AutocompleteResult($"{o.Value.Prefix}{o.Value.RealName} - {FormatDescription(o)}", o.Key));
+        var transformed = options.Where(o => !o.Value.Hide!.Value).Select(o => new AutocompleteResult($"{o.Value.Prefix}{o.Value.RealName} - {FormatDescription(o).Truncate(50)}", o.Key));
         if (string.IsNullOrEmpty(userInput))
         {
             await interaction.RespondAsync(transformed.Take(25));
