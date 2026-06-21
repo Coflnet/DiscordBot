f88a4c1437492a342d30bc002dc87f2a27f6362b
Äkwav <ekwav@coflnet.com>
2026-06-22 00:33:08 +0200

    feat: upgrade Coflnet.Core to 0.7.3 + add OpenBao config provider

--- Program.cs
+++ Program.cs
@@ -8,8 +8,11 @@ using Octokit;
 using StackExchange.Redis;
 using Coflnet.Sky.ModCommands.Client.Extensions;
 using Coflnet.Sky.ModCommands.Client.Client;
+using Coflnet.Security.OpenBao;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddOpenBaoFromEnvironment();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
